1b204534a0707db8c4ab34a4ecf990a1bc16afb3
HoangHuynh <bashaef@gmail.com>
2016-05-08 03:12:29 +0700

    This a plugin joomla
    This a plugin joomla

---
+++ h2ssigpro4phoca.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('_JEXEC') or die;
+ 
+class PlgContentH2SSigPro4Phoca extends JPlugin
+{  
+ 	public function onContentAfterSave($context, $article, $isNew)
+	{  
+		require_once JPATH_ADMINISTRATOR.'/components/com_sigpro/helper.php';
+		$phocaholder = JRequest::getString('phocafolder',false); 
+		
+		if($context=='com_phocagallery.phocagalleryimg'&&$isNew){
+			jimport('joomla.filesystem.folder');
+			$path = SigProHelper::getPath('users');
+			if(JFolder::exists($path.DIRECTORY_SEPARATOR.$phocaholder)){
+				JFolder::move($path.DIRECTORY_SEPARATOR.$phocaholder, $path.DIRECTORY_SEPARATOR.'phocagallery-'.$article->id);
+			}
+		}
+ 	}
+}
